463c9a8655e4c548af8676ee65f96edf5c971faa
christophermbe1009@gmail.com <christophermbe1009@gmail.com>
2020-10-06 19:27:50 +0100

    changed name to office test

---
+++ part3.js
@@ -0,0 +1,12 @@
+var cold = false 
+var temp = 200
+
+if (temp<40) {
+    cold=true
+    console.log('its really cold outside');
+}else if (true) {
+
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+alert('WELCOME TO BITCOINIST');
+var deposite = prompt('HOW MUCH BITCOIN WILL YOU LIKE TO DEPOSITE');
+alert('YOU HAVE DEPOSITED: '+deposite);
